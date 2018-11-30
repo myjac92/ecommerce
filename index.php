9cccb9c56840ddd93fd20db3f26b28a31a6bd4a7
josé Antônio <myjac92@gmail.com>
2018-11-29 23:26:24 -0200

    admin-categorias

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
 use \Hcode\Model\User;
+use \Hcode\Model\Category;
 
 $app = new \Slim\Slim();
 
@@ -171,6 +172,61 @@ $app->post("/admin/forgot/reset", function (){
 	]);
 	$page->setTpl("forgot-reset-success");
 });
+
+$app ->get('/admin/categories',function(){
+  User::verifyLogin();
+  $categories = category::listAll();
+  $page = new PageAdmin();
+  $page->setTpl("categories",[
+    "categories"=>$categories
+  ]);
+});
+
+$app ->get('/admin/categories/create',function(){
+  User::verifyLogin();
+  $page = new PageAdmin();
+  $page->setTpl("categories-create");
+});
+
+$app ->post('/admin/categories/create',function(){
+  User::verifyLogin();
+  $category = new Category();
+  $category->setData($_POST);
+  $category->save();
+  header("Location: /admin/categories");
+  exit;
+});
+
+$app ->get('/admin/categories/:idcategory/delete',function($idcategory){
+  User::verifyLogin();
+  $category = new Category();
+  $category->get((int)$idcategory);
+  $category->delete();
+  header("Location: /admin/categories");
+  exit;
+});
+
+$app ->get('/admin/categories/:idcategory',function($idcategory){
+  User::verifyLogin();
+  $category = new Category();
+  $category->get((int)$idcategory);
+  $page = new PageAdmin();
+  $page->setTpl("categories-update",[
+    'category'=>$category->getValues()
+  ]);
+});
+
+$app ->post('/admin/categories/:idcategory',function($idcategory){
+  User::verifyLogin();
+  $category = new Category();
+  $category->get((int)$idcategory);
+  $category->setData($_POST);
+  $category->save();
+  header("Location: /admin/categories");
+  exit;
+});
+
+
 
 
 $app->run();
